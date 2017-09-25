c5b8640fa416737542e98639584fc1e495b40401
dimakatzir <dima@artrunners.com>
2017-09-25 14:59:52 +0300

    Command line execution fixes

--- src/main/java/my/excercise/loom/log/analyzer/LogAnalyzerApplication.java
+++ src/main/java/my/excercise/loom/log/analyzer/LogAnalyzerApplication.java
@@ -3,8 +3,10 @@ package my.excercise.loom.log.analyzer;
 import my.excercise.loom.log.analyzer.data.LogAnalysisResult;
 import my.excercise.loom.log.analyzer.service.LogAnalyzer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -14,14 +16,15 @@ import java.util.stream.Stream;
 
 public class LogAnalyzerApplication {
     public static void main(String... args) throws IOException, URISyntaxException {
-        Path inputFilePath;
+        InputStreamReader inputReader;
         if (args.length == 0  || args[0].isEmpty()) {
-            inputFilePath = Paths.get(LogAnalyzerApplication.class.getClassLoader().getResource("demo.log").toURI());
+            inputReader = new InputStreamReader(
+                    LogAnalyzerApplication.class.getClassLoader().getResource("demo.log").openStream());
         } else {
-            inputFilePath = Paths.get(args[0]);
+            inputReader = new InputStreamReader(Files.newInputStream(Paths.get(args[0])));
         }
 
-        try (Stream<String> logStream = Files.lines(inputFilePath)) {
+        try (Stream<String> logStream = new BufferedReader(inputReader).lines()) {
             LogAnalyzer analyzer = new LogAnalyzer();
             LogAnalysisResult analysisResult = analyzer.analyse(logStream.collect(Collectors.toList()));
 
